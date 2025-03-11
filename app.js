48f32c5f583072083e62c9605ef3233adc1825e4
Deepa Tendulkar <tendulkardv@gmail.com>
2025-03-11 16:25:41 +0000

    Completed task 4 all tests passing, articles sorted by date with comments count

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const endpoints = require("./endpoints.json")
 const getApi = require("./controllers/api.controllers")
 const {getTopics} = require("./controllers/topics.controllers")
-const {getArticlesById} = require("./controllers/articles.controllers")
+const {getArticlesById, getArticles} = require("./controllers/articles.controllers")
 
 
 app.get('/api', getApi);
@@ -11,6 +11,8 @@ app.get('/api', getApi);
 app.get('/api/topics',getTopics)
 
 app.get('/api/articles/:article_id', getArticlesById)
+
+app.get('/api/articles', getArticles)
 
 
 //end point error
