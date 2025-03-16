d6acb8de4bbf0794c1919c832074bb0343d6714d
Deepa Tendulkar <tendulkardv@gmail.com>
2025-03-16 21:30:02 +0000

    made changes for hosting app

--- db/connection.js
+++ db/connection.js
@@ -6,11 +6,16 @@ require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
 const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("No PGDATABASE or DATABASE_URL configured")
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
+const config = {};
 
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+  }
 
-module.exports = db;
+module.exports = db(config);
